395940f370997966f4cc8911e90ec3aa575af889
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-21 17:36:46 -0300

    Criando uma lista de Produtos no produto service

--- src/app/services/produto.service.ts
+++ src/app/services/produto.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Produto } from '../models/Produto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
 
+  listaProdutos: Produto[] = [
+    {
+      id : 1,
+      nome : "iphone",
+      descricao: "celular caro",
+      valor : "100"
+    }, 
+    {
+      id : 2,
+      nome : "ipad",
+      descricao: "tablet caro",
+      valor : "200"
+    }
+  ];
+
   constructor() { }
 
-  listarProdutos(): void {
-
+  listarProdutos(): Produto[] {
+    return this.listaProdutos;
   }
 }
